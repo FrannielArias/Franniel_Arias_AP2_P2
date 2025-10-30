f0122f56db3c4ca81f1aa0184dceceaab0a0621e
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-10-29 21:11:15 -0400

    Agregando Hit, EntryPoint y permisos

--- app/src/main/java/edu/ucne/franniel_arias_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/franniel_arias_ap2_p2/MainActivity.kt
@@ -11,8 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.franniel_arias_ap2_p2.ui.theme.Franniel_Arias_AP2_P2Theme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
