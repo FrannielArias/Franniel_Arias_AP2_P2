64e3ce147f1634a5951a87838426427fe29e9f7b
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-11-05 21:28:51 -0400

    Añadiendo el RemoteDataSource y el Resource

--- app/src/main/java/edu/ucne/franniel_arias_ap2_p2/data/remote/Resource.kt
+++ app/src/main/java/edu/ucne/franniel_arias_ap2_p2/data/remote/Resource.kt
@@ -1,7 +1,5 @@
 package edu.ucne.franniel_arias_ap2_p2.data.remote
 
-import androidx.room.compiler.processing.util.Resource
-
 sealed class Resource <T>(val data: T? = null, val message: String? = null){
     class Success<T>(data: T): Resource<T>(data)
     class Error<T>(message: String, data: T? = null ): Resource<T>(data,message)
