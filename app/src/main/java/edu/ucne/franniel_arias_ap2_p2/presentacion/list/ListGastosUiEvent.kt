9cef99552649e3a3f1078f861387f5bf177177c5
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-11-08 15:17:59 -0400

    Eliminando archivos innecesarios

--- app/src/main/java/edu/ucne/franniel_arias_ap2_p2/presentacion/list/ListGastosUiEvent.kt
+++
@@ -1,5 +0,0 @@
-package edu.ucne.franniel_arias_ap2_p2.presentacion.list
-
-sealed interface ListGastosUiEvent {
-    data object GetAll : ListGastosUiEvent
-}
--- app/src/main/java/edu/ucne/franniel_arias_ap2_p2/presentacion/list/ListGastosViewModel.kt
+++
@@ -1,5 +0,0 @@
-package edu.ucne.franniel_arias_ap2_p2.presentacion.list
-
-class ListGastosViewModel {
-
-}
