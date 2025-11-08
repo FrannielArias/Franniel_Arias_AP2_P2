86544253548b3a32d5944bbca82ab51fbdd58f3d
franniel arias rodriguez <franniel_arias@ucne.edu.do>
2025-11-08 15:14:00 -0400

    Modificando use cases

--- app/src/main/java/edu/ucne/franniel_arias_ap2_p2/domain/useCase/SaveGastosUseCase.kt
+++ app/src/main/java/edu/ucne/franniel_arias_ap2_p2/domain/useCase/SaveGastosUseCase.kt
@@ -1,17 +1,14 @@
 package edu.ucne.franniel_arias_ap2_p2.domain.useCase
 
-import edu.ucne.franniel_arias_ap2_p2.data.remote.Resource
 import edu.ucne.franniel_arias_ap2_p2.domain.model.Gastos
 import edu.ucne.franniel_arias_ap2_p2.domain.repository.GastosRepository
-import kotlinx.coroutines.flow.Flow
+import java.lang.Exception
 import javax.inject.Inject
 
-
-class GetGastoUseCase @Inject constructor(
+class SaveGastosUseCase @Inject constructor(
     private val repository: GastosRepository
 ) {
-    operator fun invoke(id: Int): Flow<Resource<List<Gastos>>> {
-        return repository.getGasto(id)
-
+    suspend operator fun invoke(gastos: Gastos){
+        repository.saveGastos(gastos)
     }
 }
